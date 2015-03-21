32cb3183fc5f854b77460656d32552fde4f2a97e
Pasindu Perera <pasindup@leapset.com>
2015-03-21 21:59:28 +0530

    omnibox suggestion select linkn click added

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -33,4 +33,8 @@ chrome.omnibox.onInputChanged.addListener(
 chrome.omnibox.onInputEntered.addListener(
     function (text) {
         console.debug('Selected : ' + text);
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {data: text});
+        });
+
     });
